30feeb82a2ab40808d8e0fffbb4b3344386fbfd4
Matheus Schneider <matheus.ssm@gmail.com>
2020-06-14 15:40:06 -0300

    Added moongoDb integration, created initial model

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://user_omnistack:omnistack@cluster0-alt5r.mongodb.net/aircnc?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 app.use(express.json());
 app.use(routes);
