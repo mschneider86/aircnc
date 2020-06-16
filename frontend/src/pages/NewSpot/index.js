4af88fb7270c4ac65a2e8f77618a9e0860d8dfe2
Matheus Schneider <matheus.ssm@gmail.com>
2020-06-16 19:20:38 -0300

    Created NewSpot page styling and structure

--- frontend/src/pages/NewSpot/index.js
+++ frontend/src/pages/NewSpot/index.js
@@ -1,5 +1,64 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+
+import camera from "../../assets/camera.svg";
+import "./styles.css";
 
 export default function NewSpot() {
-  return <div></div>;
+  const [company, setCompany] = useState("");
+  const [techs, setTechs] = useState("");
+  const [price, setPrice] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+
+  const preview = useMemo(() => {
+    return thumbnail ? URL.createObjectURL(thumbnail) : null;
+  }, [thumbnail]);
+
+  function handleSubmit() {}
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label
+        id="thumbnail"
+        style={{ backgroundImage: `url(${preview})` }}
+        className={thumbnail ? "has-thumbnail" : ""}
+      >
+        <input
+          type="file"
+          onChange={(event) => setThumbnail(event.target.files[0])}
+        />
+        <img src={camera} alt="Thumbnail" />
+      </label>
+      <label htmlFor="company">Empresa *</label>
+      <input
+        id="company"
+        placeholder="Sua empresa incrível"
+        value={company}
+        onChange={(event) => setCompany(event.target.value)}
+      />
+
+      <label htmlFor="techs">
+        Tecnologias * <span>(separadas por vírgula)</span>
+      </label>
+      <input
+        id="techs"
+        placeholder="Quais tecnologias usam?"
+        value={techs}
+        onChange={(event) => setTechs(event.target.value)}
+      />
+
+      <label htmlFor="price">
+        Valor da diária * <span>(em branco para GRATUITO)</span>
+      </label>
+      <input
+        id="price"
+        placeholder="Valor cobrado por dia"
+        value={price}
+        onChange={(event) => setPrice(event.target.value)}
+      />
+
+      <button type="submit" className="btn">
+        Cadastrar
+      </button>
+    </form>
+  );
 }
