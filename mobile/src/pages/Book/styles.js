1523dafb103b2d92852be8785a22977b19f15ddb
Matheus Schneider <matheus.ssm@gmail.com>
2020-06-17 19:44:36 -0300

    Added Login page styling

--- mobile/src/pages/Book/styles.js
+++ mobile/src/pages/Book/styles.js
@@ -1,3 +1,5 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({});
+const styles = StyleSheet.create({});
+
+export default styles;
--- mobile/src/pages/Login/index.js
+++ mobile/src/pages/Login/index.js
@@ -1,12 +1,51 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  KeyboardAvoidingView,
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
 
 import styles from "./styles";
 
+import logo from "../../assets/logo.png";
+
 export default function Login() {
   return (
-    <View>
-      <Text>Hello Login!</Text>
-    </View>
+    <KeyboardAvoidingView
+      enabled={Platform.OS === "ios"}
+      behavior="padding"
+      style={styles.container}
+    >
+      <Image source={logo} />
+
+      <View style={styles.form}>
+        <Text style={styles.label}>SEU E-MAIL *</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Seu e-mail"
+          placeholderTextColor="#999"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+
+        <Text style={styles.label}>TECNOLOGIAS *</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Tecnologias de interesse"
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Encontrar spots</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
